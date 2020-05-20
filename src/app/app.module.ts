c46a68b3b4f91bb7a58357025941f674d29c39a3
ayoubTig <tigoudrne@gmail.com>
2020-05-20 18:11:34 +0000

    hiiiii

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./app.routes";
+import {TopbarComponent} from "./components/topbar/topbar.component";
+import {CartService} from "./services/cart.service";
+import {CartPopupComponent} from "./pages/cart/cart-popup/cart-popup.component";
+import {ProductService} from "./services/products.service";
+import { GestionUsersComponent } from './pages/admin/gestion_users/gestion_users.component';
+import { Users } from './services/user';
+ 
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TopbarComponent,
+        CartPopupComponent 
+         
+        
+         
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [CartService,ProductService,Users],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
